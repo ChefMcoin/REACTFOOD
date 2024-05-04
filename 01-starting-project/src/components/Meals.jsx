85caaaa291ee15b3e4cef37b6f608b877dba0614
Mustapha <mustaphabakare2@gmail.com>
2024-05-04 13:55:09 +0100

    new

---
+++ 01-starting-project/src/components/Meals.jsx
@@ -0,0 +1,34 @@
+import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {};
+
+export default function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+    sendRequest,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
+
+  //   if (!data) {
+  //     return <p>No meals found...</p>;
+  //   }
+
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error} />;
+  }
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem meal={meal} key={meal.id} />
+      ))}
+    </ul>
+  );
+}
